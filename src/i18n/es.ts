c51ded266a8620320c09c138492b3422d0433e6f
Alberto <albertw042z@gmail.com>
2026-05-11 17:44:22 -0600

    feat: add hero, about and projects components

--- src/i18n/es.ts
+++ src/i18n/es.ts
@@ -4,5 +4,9 @@ export const es = {
         about: 'Sobre mí',
         projects: 'Proyectos',
         contact: 'Contacto'
+    },
+    hero:{
+        title: 'Hola, Soy',
+        description: 'Me especializo en crear sitios web modernos y receptivos utilizando tecnologías como Astro, React, Tailwind CSS y TypeScript. Mi objetivo es construir experiencias digitales atractivas y funcionales que satisfagan las necesidades de los usuarios.'
     }
 };
